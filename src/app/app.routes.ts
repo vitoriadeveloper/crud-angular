8af84fbc61ca17ddc3c15b7cd0996f73648e6727
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2024-08-15 17:49:37 -0300

    refactor: criação de rota de cadastro de produto

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,9 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { RegisterComponent } from './pages/register/register.component';
 
 export const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
 ];
