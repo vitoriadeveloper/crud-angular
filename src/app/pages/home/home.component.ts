67caed76ec98655f2fc757deafe655e30925a4b6
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2024-08-16 12:31:23 -0300

    refactor: alteração de nome de card para table

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RegisterComponent } from '../register/register.component';
-import { CardComponent } from "../../components/card/card.component";
+import { TableComponent } from '../../components/table/table.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [RegisterComponent, CardComponent],
+  imports: [RegisterComponent, TableComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
