963aed4e885c574a9bf2b98ddedb64d736670eb9
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2024-08-16 12:31:00 -0300

    refactor: configuração para mascara

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,20 @@
-import { ApplicationConfig } from '@angular/core';
+import { ApplicationConfig, LOCALE_ID } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+import { registerLocaleData } from '@angular/common';
+import localePt from '@angular/common/locales/pt';
+
+import { provideNgxMask } from 'ngx-mask';
+
+registerLocaleData(localePt, 'pt-BR');
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimationsAsync()]
+  providers: [
+    provideRouter(routes),
+    provideAnimationsAsync(),
+    { provide: LOCALE_ID, useValue: 'pt-BR' },
+    provideNgxMask()
+  ],
 };
