6de48e4aa390fbeca9336529a09a6acc9b491a38
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2024-08-16 13:06:07 -0300

    refactor: convertendo service para chamadas async

--- src/app/service/product.service.ts
+++ src/app/service/product.service.ts
@@ -1,41 +1,61 @@
 import { Injectable } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { Product } from '../models/product';
 
 @Injectable({
   providedIn: 'root'
 })
-
 export class ProductService {
-private storageKey  = 'items';
+  private storageKey = 'items';
 
-getItem(): Product[]{
-const savedItems = sessionStorage.getItem(this.storageKey);
-return savedItems ? JSON.parse(savedItems) : [];
-}
-
-saveItems (items: Product[]):void{
-sessionStorage.setItem(this.storageKey, JSON.stringify(items));
-}
-
-addItem(item: any){
-const items = this.getItem();
-items.push(item);
-this.saveItems(items);
-}
-
-updateItem(index: number, item: Product): void {
-  const items = this.getItem();
-  if(index >= 0 && index < items.length){
-    items[index] = item;
-    this.saveItems(items);
+  getItem(): Observable<Product[]> {
+    const savedItems = sessionStorage.getItem(this.storageKey);
+    const items = savedItems ? JSON.parse(savedItems) : [];
+    return of(items);
   }
-}
 
-  deleteItem(index: number): void {
-    const items = this.getItem();
-    if(index >= 0 && index < items.length){
-      items.splice(index, 1);
-      this.saveItems(items);
-    }
+  saveItems(items: Product[]): void {
+    sessionStorage.setItem(this.storageKey, JSON.stringify(items));
+  }
+
+  addItem(item: Product): Observable<void> {
+    return new Observable(observer => {
+      const items = this.getItem().subscribe(existingItems => {
+        existingItems.push(item);
+        this.saveItems(existingItems);
+        observer.next();
+        observer.complete();
+      });
+    });
+  }
+
+  updateItem(index: number, item: Product): Observable<void> {
+    return new Observable(observer => {
+      this.getItem().subscribe(items => {
+        if (index >= 0 && index < items.length) {
+          items[index] = item;
+          this.saveItems(items);
+          observer.next();
+          observer.complete();
+        } else {
+          observer.error('Índice fora dos limites');
+        }
+      });
+    });
+  }
+
+  deleteItem(index: number): Observable<void> {
+    return new Observable(observer => {
+      this.getItem().subscribe(items => {
+        if (index >= 0 && index < items.length) {
+          items.splice(index, 1);
+          this.saveItems(items);
+          observer.next();
+          observer.complete();
+        } else {
+          observer.error('Índice fora dos limites');
+        }
+      });
+    });
   }
 }
