27b92b58e8354a9ba9a305907f9719b7e183efa7
Vitoria Carolina <92760249+vitoriadeveloper@users.noreply.github.com>
2024-08-16 13:08:17 -0300

    refactor: adição de toasts

--- src/app/components/table/table.component.ts
+++ src/app/components/table/table.component.ts
@@ -1,39 +1,66 @@
-import { CommonModule  } from '@angular/common';
+import { CommonModule } from '@angular/common';
 import { Component, inject, OnInit } from '@angular/core';
-import { ProductService } from '../../service/product.service';
 import { Router } from '@angular/router';
+import { ProductService } from '../../service/product.service';
 import { Product } from '../../models/product';
-import { BrDatePipe } from '../../shared/br-date.pipe';
+import { ToastrService } from 'ngx-toastr';
+import { catchError, of, tap } from 'rxjs';
 
 @Component({
   selector: 'app-table',
   standalone: true,
-  imports: [CommonModule, BrDatePipe],
+  imports: [CommonModule],
   templateUrl: './table.component.html',
-  styleUrl: './table.component.scss'
+  styleUrls: ['./table.component.scss']
 })
-export class TableComponent implements OnInit{
+export class TableComponent implements OnInit {
   products: Product[] = [];
   private route = inject(Router);
   private productService = inject(ProductService);
+  private toastr = inject(ToastrService);
 
   ngOnInit(): void {
     this.getProducts();
   }
 
-  getProducts():void {
-    this.products = this.productService.getItem();
+  getProducts(): void {
+    this.productService.getItem().pipe(
+      tap(items => this.products = items),
+      catchError(error => {
+        this.showError();
+        return of([]);
+      })
+    ).subscribe();
   }
 
   editProduct(i: number): void {
     this.route.navigate(['/register', i]);
   }
 
-  deleteProduct(i: number): void {
-    if (confirm('Você tem certeza que deseja excluir este item?')) {
-      this.productService.deleteItem(i);
-    }
-    this.getProducts();
+  showSuccess(): void {
+    this.toastr.success('Operação realizada com sucesso!', 'Sucesso', {
+      toastClass: 'toast toast-success'
+    });
   }
 
+  showError(): void {
+    this.toastr.error('Ocorreu um erro!', 'Erro', {
+      toastClass: 'toast toast-error'
+    });
+  }
+
+  deleteProduct(i: number): void {
+    if (confirm('Você tem certeza que deseja excluir este item?')) {
+      this.productService.deleteItem(i).pipe(
+        tap(() => {
+          this.showSuccess();
+          this.getProducts();
+        }),
+        catchError(error => {
+          this.showError();
+          return of();
+        })
+      ).subscribe();
+    }
+  }
 }
